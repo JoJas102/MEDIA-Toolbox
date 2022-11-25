cce0086dbda1cdc60fd6beb1352917d5b793ba20
JJ <49498024+JoJas102@users.noreply.github.com>
2022-11-25 11:50:37 +0100

    Snakify InitParam -testing

--- InitParam.py
+++ InitParam.py
@@ -1,22 +1,19 @@
 # Initial parameters are set here
 
-    format long
+import math
+import numpy as np
+import nibabel as nib
 
-    # Initialize variables
-    addpath('.\NIfTI_20140122')
-    filepath = {'.\test data\scans4Db16.nii' '.\test data\segmentationC.nii'}
-    nii = load_nii(filepath{1}) # load nii as double matrix with dim neq 1
-    nii = squeeze(double(nii.img))
-    ROI = load_nii(filepath{2})
-    b = [0,5,10,20,30,40,50,75,100,150,200,250,300,400,525,750]
+# Initialize variables
+files = ['.\test data\scans4Db16.nii' '.\test data\segmentationC.nii']
+nii = nib.load(files[1])
+nii = np.squeeze(nii)
+ROI = nib.load(files[2])
+b = [0,5,10,20,30,40,50,75,100,150,200,250,300,400,525,750]
 
-    # Generate basis values
-    Dmin = 0.7*1e-3       # [vanBaalen2016] & [Wong2019]
-    Dmax = 300*1e-3
-    m = 300               # no. of bins             
-    DValues = logspace(log10(Dmin), log10(Dmax), m) 
-    DBasis = exp(-kron( b', DValues))
-
-    # Define total diffusion signal decay according to sum of f_i*e^(-b*D_i)
-    #A = exp(-kron(b', d)) # constraint matrix containing exp decay funxtions
-    #rawSignal = A*f'      # mean signal of ROI/voxel without noise
+# Generate basis values
+Dmin = 0.7*1e-3       # [vanBaalen2016] & [Wong2019]
+Dmax = 300*1e-3
+m = 300               # no. of bins             
+DValues = np.logspace(np.log10(Dmin), np.log10(Dmax), m) 
+DBasis = math.exp(-math.kron(np.transpose(b), DValues))
