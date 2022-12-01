2efb7b5e9771465fedaf093793710552bda65fbc
JJ <49498024+JoJas102@users.noreply.github.com>
2022-12-01 07:48:34 +0100

    Deleted description

--- MEDIA_toolbox.py
+++ MEDIA_toolbox.py
@@ -3,50 +3,6 @@
 #           Multi-Exponential Decay Image Analysis (MEDIA) Toolbox
 #
 ###########################################################################
-# region <Discription>
-# General information
-###########################################################################
-#
-# Program to analyse multi-exponential signal compartments in DWI images/scans
-# with several b-values using NNLS fitting of the diffusion data and compare
-# multi-exponential fitting methods. Finding the total number of components
-# contributing to the corresponding multi-exponential signal and analysing
-# the results by calculating corresponding diffusion parameters.
-# This fitting routine uses the regularized NNLS algorithm with cross
-# validation (CVNNLS.m) from Thorarin Bjarnason for comparison.
-#
-# Generally capital letters represent matrices or tensors and small letters
-# stand for variuos variables, vectors and constants.
-#
-# To carry out this simulation the following functions need to be located
-# in the MATLAB path:
-# InitVar.m
-# load_nii.m (NIfTI package)
-# NNLSfitting.m
-# CVNNLS.m
-# fastnnls.m
-# findpeaksNNLS.m
-# plotSimu.m
-# write3DMatrixToTxt.m
-#
-# For any further comments or explaining descriptions see annotations
-# inside the functions code files.
-#
-# -
-# Jonas Jasse
-# Last modified 18.04.2022
-#
-###########################################################################
-# Initial variables (InitVar.m)
-###########################################################################
-#
-# - filepaths{} = cell array containing filepaths to scan image (4D NIfTI)
-#   and ROI (segmentation containing region of interest as NIfTI)
-# - b = list of used b-values for plotting
-# - DValues = fitting range
-# - m = number of bins for fitting procedure
-# endregion
-###########################################################################
 # Init parameters
 ###########################################################################
 
@@ -58,7 +14,7 @@ from InitParam import b, nii, ROI, DValues, DBasis, Dmin, Dmax
 # Read NIfTIs, cut out ROIs and extract mean signal
 ###########################################################################
 
-# TODO: cut ROI and extract signal from NIFTIs
+# TODO: test cutting ROI and extracting signal from NIFTIs
 signal = np.multiply(nii, np.tile(ROI.img, ([1, 1, len(b)])))
 meanSignal = np.squeeze(np.mean(signal)).T
 
