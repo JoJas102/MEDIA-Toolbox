87a89a50b7b94f81d605728fcb574b60c985874a
JJ <49498024+JoJas102@users.noreply.github.com>
2022-12-01 07:01:01 +0100

    Last formatting steps

--- InitParam.py
+++ InitParam.py
@@ -14,6 +14,6 @@ b = [0, 5, 10, 20, 30, 40, 50, 75, 100, 150, 200, 250, 300, 400, 525, 750]
 # Generate basis values
 Dmin = 0.7 * 1e-3  # [vanBaalen2016] & [Wong2019]
 Dmax = 300 * 1e-3
-m = 300     # no. of bins
+m = 300  # no. of bins
 DValues = np.logspace(np.log10(Dmin), np.log10(Dmax), m)
 DBasis = math.exp(-math.kron(b.T), DValues)
