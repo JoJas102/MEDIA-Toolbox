6a5389b17ce67594c4cbc9894c16e166b5d21f28
JJ <49498024+JoJas102@users.noreply.github.com>
2022-12-01 10:34:35 +0100

    Done correction and testing

--- InitParam.py
+++ InitParam.py
@@ -1,19 +1,20 @@
 # Initial parameters are set here
 
-import math
 import numpy as np
 import nibabel as nib
 
 # Initialize variables
-files = [r".\test data\scans4Db16.nii" r".\test data\segmentationC.nii"]
-nii = nib.load(files[1])
+
+files = ["./testData/scans4Db16.nii", "./testData/segmentationC.nii"]
+nii = nib.load(files[0])
 nii = np.squeeze(nii)
-ROI = nib.load(files[2])
-b = [0, 5, 10, 20, 30, 40, 50, 75, 100, 150, 200, 250, 300, 400, 525, 750]
+ROI = nib.load(files[1])
+b = np.array([[0, 5, 10, 20, 30, 40, 50, 75, 100, 150, 200, 250, 300, 400, 525, 750]])
 
 # Generate basis values
+
 Dmin = 0.7 * 1e-3  # [vanBaalen2016] & [Wong2019]
 Dmax = 300 * 1e-3
 m = 300  # no. of bins
-DValues = np.logspace(np.log10(Dmin), np.log10(Dmax), m)
-DBasis = math.exp(-math.kron(b.T), DValues)
+DValues = np.array(np.logspace(np.log10(Dmin), np.log10(Dmax), m))
+DBasis = np.exp(-np.kron(b.T, DValues))
