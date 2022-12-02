f019afc36027cd14b966ecc471fa2ada286444a6
JJ <49498024+JoJas102@users.noreply.github.com>
2022-12-02 11:33:14 +0100

    Adjusted code for pixelwise analysis

--- findpeaksNNLS.py
+++ findpeaksNNLS.py
@@ -7,32 +7,32 @@ from scipy.signal import peak_widths
 def findpeaksNNLS(s, DValues):
     # find peaks and diffusion coefficients of NNLS fitting results
 
-    # Matrix indices at iteration i
-    # [1,i] slow/tissue
-    # [2,i] inter/tubular
-    # [3,i] fast/blood
+    for i in len(s):
+        for j in len(s):
 
-    # [maxima,dNNLS,widths,proms] = findpeaks(s, 1:length(DValues), 'SortStr', 'descend', 'WidthReference','halfheight') # returns maxima, x positions, FWHM and prominences of peaks
+            # TODO: descending output?!
+            # TODO: thresholding possible?
+            d, properties = find_peaks(s[i][j][:])
+            fwhm, _ = peak_widths(s[i][j][:], d, rel_height=0.5)
+            maxima = properties.peak_heights
 
-    d, properties = find_peaks(s)  # TODO: descending output?!
-    fwhm, _ = peak_widths(s, d, rel_height=0.5)
-    maxima = properties.peak_heights
+            # Convert back to log scale values
+            d = DValues(d)
 
-    d = DValues(d)  # convert back to log scale values
+            # Calc area under gaussian curve
+            f = (
+                np.multiply(maxima, fwhm)
+                / (2 * math.sqrt(2 * math.log(2)))
+                * math.sqrt(2 * math.pi)
+            )
 
-    # Calc area under gaussian curve
-    f = (
-        np.multiply(maxima, fwhm)
-        / (2 * math.sqrt(2 * math.log(2)))
-        * math.sqrt(2 * math.pi)
-    )
+            # Threshold
+            d[f < 0.03] = 0  # remove any entry with vol frac lower 3%
+            # TODO: obsolet code if threshold/prominence adjusted in find_peaks
+            f[f < 0.03] = 0
 
-    # Threshold
-    d[f < 0.03] = 0  # remove any entry with vol frac lower 3%
-    # TODO: obsolet code if threshold/prominence adjusted in find_peaks
-    f[f < 0.03] = 0
+            f = np.divide(f, sum(f)) * 100  # normalize f
 
-    f = np.divide(f, sum(f)) * 100  # normalize f
+            result = [maxima, np.transpose(d), np.transpose(fwhm), f]
 
-    result = [maxima, np.transpose(d), np.transpose(fwhm), f]
     return d, f, result
