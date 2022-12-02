22f8ba59873d09320f9e4ae2ac77a49f0dd88e3b
JJ <49498024+JoJas102@users.noreply.github.com>
2022-12-02 11:33:07 +0100

    Adjusted code for pixelwise analysis

--- NNLSfitting.py
+++ NNLSfitting.py
@@ -1,15 +1,18 @@
+import numpy as np
 from scipy.optimize import nnls
 
 
 def NNLSfitting(DBasis, signal):
 
+    s, sReg = np.empty(len(signal), len(signal), len(DBasis))
+
     for i in len(signal):
         for j in len(signal):
             # NNLS fitting w\ reg minimises norm(A*s-signal) for reference
-            s = nnls(DBasis, signal[i][j][:])
+            s[i][j][:] = np.array(nnls(DBasis, signal[i][j][:]))
 
-            # NNLS fitting w reg
-            [sReg, mu, resid] = CVNNLS(DBasis, signal[i][j][:])  # CVNNLS from Bjarnason
+            # NNLS fitting w reg (CVNNLS from Bjarnason)
+            sReg[i][j][:], mu, resid = np.array(CVNNLS(DBasis, signal[i][j][:]))
             # larger mu = more satisfaction of constraints at expanse of increasing misfit (Witthal1989)
 
     return s, sReg, mu
