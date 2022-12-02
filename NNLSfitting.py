d8fcf849cce0127e07ff30580efa8f2daaadca22
JJ <49498024+JoJas102@users.noreply.github.com>
2022-12-02 11:19:25 +0100

    Adjusted code for pixelwise analysis

--- NNLSfitting.py
+++ NNLSfitting.py
@@ -3,11 +3,13 @@ from scipy.optimize import nnls
 
 def NNLSfitting(DBasis, signal):
 
-    # NNLS fitting w\ Reg (find unknown signal amplitude of components (signal)) minimises norm(A*s-signal) for reference
-    s = nnls(DBasis, signal)
+    for i in len(signal):
+        for j in len(signal):
+            # NNLS fitting w\ reg minimises norm(A*s-signal) for reference
+            s = nnls(DBasis, signal[i][j][:])
 
-    # Regularization fitting NNLS
-    [s2, mu, resid] = CVNNLS(DBasis, signal)  # CVNNLS from Bjarnason
-    # larger mu = more satisfaction of constraints at expanse of increasing misfit (Witthal1989)
+            # NNLS fitting w reg
+            [sReg, mu, resid] = CVNNLS(DBasis, signal[i][j][:])  # CVNNLS from Bjarnason
+            # larger mu = more satisfaction of constraints at expanse of increasing misfit (Witthal1989)
 
-    return s, s2, mu
+    return s, sReg, mu
