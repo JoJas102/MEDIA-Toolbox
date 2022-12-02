0ba359096e58978f287460ed1b891296073a2425
JJ <49498024+JoJas102@users.noreply.github.com>
2022-12-02 11:15:54 +0100

    Simplified code

--- findpeaksNNLS.py
+++ findpeaksNNLS.py
@@ -4,16 +4,8 @@ from scipy.signal import find_peaks
 from scipy.signal import peak_widths
 
 
-def findpeaksNNLS(matrix, reg):
-
-    # statistics for fit with (reg = 1) and w\ regression (0)
-    if reg == 1:
-        s = matrix[3, :, :]  # sNNLSReg
-    else:
-        s = matrix[2, :, :]  # sNNLSNoReg
-
-    s = np.transpose(s)  # right array dimensions
-    DValues = matrix[1, :, 1]
+def findpeaksNNLS(s, DValues):
+    # find peaks and diffusion coefficients of NNLS fitting results
 
     # Matrix indices at iteration i
     # [1,i] slow/tissue
@@ -22,25 +14,25 @@ def findpeaksNNLS(matrix, reg):
 
     # [maxima,dNNLS,widths,proms] = findpeaks(s, 1:length(DValues), 'SortStr', 'descend', 'WidthReference','halfheight') # returns maxima, x positions, FWHM and prominences of peaks
 
-    dNNLS, properties = find_peaks(s)  # TODO: descending output?!
-    fwhm, _ = peak_widths(s, dNNLS, rel_height=0.5)
+    d, properties = find_peaks(s)  # TODO: descending output?!
+    fwhm, _ = peak_widths(s, d, rel_height=0.5)
     maxima = properties.peak_heights
 
-    dNNLS = DValues(dNNLS)  # convert back to log scale values
+    d = DValues(d)  # convert back to log scale values
 
     # Calc area under gaussian curve
-    fNNLS = (
+    f = (
         np.multiply(maxima, fwhm)
         / (2 * math.sqrt(2 * math.log(2)))
         * math.sqrt(2 * math.pi)
     )
 
     # Threshold
-    dNNLS[fNNLS < 0.03] = 0  # remove any entry with vol frac lower 3%
+    d[f < 0.03] = 0  # remove any entry with vol frac lower 3%
     # TODO: obsolet code if threshold/prominence adjusted in find_peaks
-    fNNLS[fNNLS < 0.03] = 0
+    f[f < 0.03] = 0
 
-    fNNLS = np.divide(fNNLS, sum(fNNLS)) * 100  # normalize f
+    f = np.divide(f, sum(f)) * 100  # normalize f
 
-    resultNNLS = [maxima, np.transpose(dNNLS), np.transpose(fwhm), fNNLS]
-    return dNNLS, fNNLS, resultNNLS
+    result = [maxima, np.transpose(d), np.transpose(fwhm), f]
+    return d, f, result
