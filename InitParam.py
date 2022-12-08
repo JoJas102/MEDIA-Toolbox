46c55a6b5205ddb3877738f6602b02bd1ac43ab1
JJ <49498024+JoJas102@users.noreply.github.com>
2022-12-08 10:25:21 +0100

    Smoother code: acquired

--- InitParam.py
+++ InitParam.py
@@ -4,7 +4,6 @@ import numpy as np
 import nibabel as nib
 
 # Initialize variables
-
 files = ["./testData/scans4Db16.nii", "./testData/segmentationC.nii"]
 nii = nib.load(files[0])
 nii = np.array(nii.get_data())
@@ -13,9 +12,7 @@ ROI = np.array(nib.load(files[1]).get_data())
 b = np.array([[0, 5, 10, 20, 30, 40, 50, 75, 100, 150, 200, 250, 300, 400, 525, 750]])
 
 # Generate basis values
-
-Dmin = 0.7 * 1e-3  # [vanBaalen2016] & [Wong2019]
-Dmax = 300 * 1e-3
+Dmin, Dmax = 0.7 * 1e-3, 300 * 1e-3  # [vanBaalen2016] & [Wong2019]
 m = 300  # no. of bins
 DValues = np.array(np.logspace(np.log10(Dmin), np.log10(Dmax), m))
 DBasis = np.exp(-np.kron(b.T, DValues))
