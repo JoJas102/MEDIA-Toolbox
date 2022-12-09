83e6cc59ecadcf3b0c5a7b137e5b4161e2adc9ca
JJ <49498024+JoJas102@users.noreply.github.com>
2022-12-09 12:15:11 +0100

    Fixed dim error with b

--- InitParam.py
+++ InitParam.py
@@ -15,4 +15,4 @@ b = np.array([0, 5, 10, 20, 30, 40, 50, 75, 100, 150, 200, 250, 300, 400, 525, 7
 Dmin, Dmax = 0.7 * 1e-3, 300 * 1e-3  # [vanBaalen2016] & [Wong2019]
 m = 300  # no. of bins
 DValues = np.array(np.logspace(np.log10(Dmin), np.log10(Dmax), m))
-DBasis = np.exp(-np.kron(b.T, DValues))
+DBasis = np.exp(-np.kron(np.array([b]).T, DValues))
